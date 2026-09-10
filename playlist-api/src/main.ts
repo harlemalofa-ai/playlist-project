cef1ea880111c1c8e203f8d331e7031a2a299584
harlemalofa-ai <harlemalofa@gmail.com>
2026-09-10 16:07:02 +0200

    Enable global DTO validation

--- playlist-api/src/main.ts
+++ playlist-api/src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -5,6 +6,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
 
   await app.listen(process.env.APP_PORT ?? 3000);
 }
